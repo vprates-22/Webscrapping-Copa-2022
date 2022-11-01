cb91d19f79a77dd8e4c5ed1867a2c1994b8401de
vprates-22 <vprates2222@gmail.com>
2022-11-01 19:43:36 -0300

    01/11/2022

--- webscrapper.py
+++ webscrapper.py
@@ -27,7 +27,7 @@ for i in range(len(Time)):
     j=1
     while(True):
         # Acessando um jogo específico
-        dataLimite = datetime(2022, 8, 2)
+        dataLimite = datetime(2022, 10, 31)
         jogo, dataJogo = module.acessaJogo(driver, actions, j, dataLimite)
         if(jogo == 1):
             resultado = module.acessaEscalação(driver, actions, Time[i])
